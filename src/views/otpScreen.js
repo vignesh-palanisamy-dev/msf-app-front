f882f9b456bf25b0b5ca705228caf39fb2818308
yampvideochat <vignesh.gobitbyte@gmail.com>
2020-12-07 02:56:22 +0530

    parse int otp commit

--- src/views/otpScreen.js
+++ src/views/otpScreen.js
@@ -78,7 +78,7 @@ export default function Otp(props) {
     if(isValidateError){
         return;
       }
-      if(otp !== secretOtp){
+      if(parseInt(otp) !== secretOtp){
         toast.error("Invalid OTP.");
         return;
       }
